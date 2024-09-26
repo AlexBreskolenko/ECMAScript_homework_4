137217d7962cfc536e2e38d111b2eb882364f983
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-26 18:05:26 +0300

    Task 1 completed

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+async function getUserData(ID) {
+  const responce = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await responce.json();
+  if (responce.ok) {
+    const user = users.find((ell) => ell.id === ID);
+    console.log(user === undefined ? "Пользователь не найден" : user);
+  } else {
+    console.log(`Error : ${responce.status}`);
+  }
+}
 
+getUserData(2);
