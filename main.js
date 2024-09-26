20cba45193986fc61c3c16cb22517e21e26eec39
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-09-26 19:06:06 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ********************Task 1
 async function getUserData(ID) {
   const responce = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await responce.json();
@@ -10,3 +11,28 @@ async function getUserData(ID) {
 }
 
 getUserData(2);
+
+// *************Task 2
+const user = {
+  name: "Alex",
+  surname: "Belkin",
+  age: "30",
+  email: "alex@gmail.com",
+};
+
+async function saveUserData(url, user) {
+  let responce = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charser=utf-8",
+    },
+    body: JSON.stringify(user),
+  });
+  if (responce.ok) {
+    console.log(responce.status);
+  } else {
+    console.log("Что то пошло не так.");
+  }
+}
+
+// **************Task 3
